40dab1dc94baf02964b44738cddadb4f13c9b467
Kevin Febrian <pownyfals@gmail.com>
2022-04-01 17:15:20 +0700

    menambahkan class matkul_1302204081 dan json 2

--- tpmodul6_kelompok_rojakSquad/kevin_1302204081.cs
+++ tpmodul6_kelompok_rojakSquad/kevin_1302204081.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.
+
 
 namespace tpmodul6_kelompok_rojakSquad
 {
@@ -15,6 +15,12 @@ namespace tpmodul6_kelompok_rojakSquad
 		public string nama { set; get; }
 		public int nim { set; get; }
 		public string fakultas {set;get;}
+	}
+
+	public class matkul_1302204081
+	{
+		public string kodeMatkul { get; set; }
+		public string namaMatkul{ set; get; }
 	}
 	public class Program
 	{
@@ -29,7 +35,11 @@ namespace tpmodul6_kelompok_rojakSquad
 			};
 			var kevin = JsonSerializer.Deserialize<kevin_1302204081>(json);
 
-			Console.WriteLine(kevin.nim);
+			Console.WriteLine($"nama {kevin.nama} dengan nim {kevin.nim} dari fakultas {kevin.fakultas}");
+
+			var json2 = @"C:\College\Programming\Konstruksi Perangkat Lunak\TP\TP6
+			\tpmodul6_kelompok_rojakSquad\tpmodul6_kelompok_rojakSquad\tp6_2_1302204081.json";
+			var matkul = JsonSerializer.Deserialize<matkul_1302204081>(json2);
 		}
 	}
 
