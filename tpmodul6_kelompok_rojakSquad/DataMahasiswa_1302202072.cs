6a6353ef6d84ae4bf87d39c9ec184d31c3a071bd
razqyaqil <razqyaqil2@gmail.com>
2022-04-01 16:52:41 +0700

    “menambahkan class DataMahasiswa_1302202072

---
+++ tpmodul6_kelompok_rojakSquad/DataMahasiswa_1302202072.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+using System.Net.Http.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace tpmodul6_kelompok_rojakSquad
+{
+    internal class DataMahasiswa_1302202072
+    {
+        public void readJson()
+        {
+            StreamReader r = new StreamReader("tp6_1_1302202072.json");
+            string jsonString = r.ReadToEnd();
+            JObject parsed = JObject.Parse(jsonString);
+
+            var nama = parsed["nama"];
+            var depan = nama["depan"].Value<String>();
+            var belakang = nama["belakang"].Value<String>();
+
+            Console.WriteLine("Nama {0} {1} dengan nim {2} dari fakultas {3} ",depan,belakang, parsed["nim"].Value<String>(), parsed["fakultas"].Value<String>());
+
+
+
+            
+        }
+    }
+}
--- tpmodul6_kelompok_rojakSquad/Program.cs
+++ tpmodul6_kelompok_rojakSquad/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace tpmodul6_kelompok_rojakSquad // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            DataMahasiswa_1302202072 r = new DataMahasiswa_1302202072();
+            r.readJson();
+        }
+    }
+}
